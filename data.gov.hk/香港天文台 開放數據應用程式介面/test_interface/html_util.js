4e2a7b35e6929d9746f3a02998006a2387065b0c
louiscklaw <louiscklaw@gmail.com>
2019-11-27 20:10:07 +0800

    update,

---
+++ data.gov.hk/香港天文台 開放數據應用程式介面/test_interface/html_util.js
@@ -0,0 +1,78 @@
+
+const SELECT_YEAR = _.range(2019,2021+1)
+const SELECT_MONTH = _.range(1,12+1)
+const SELECT_DAY = _.range(1,31+1)
+const SELECT_HOUR = _.range(1,24+1)
+
+const translateValue = (value) => value == ''? '-': value
+
+const wrapTag = (tag_to_wrap, text, class_in, id_in) => {
+  // TODO: class_string not implemented
+  let class_string = class_in==null?'': `id="${class_in}"`
+  let id_string = id_in==null?'': `id="${id_in}"`
+  return `<${tag_to_wrap} ${class_string} ${id_string} >${text}</${tag_to_wrap}>`
+}
+
+function getOption (text) { return wrapTag('option', text) }
+
+function getTr(text) { return wrapTag('tr', text) }
+function getTd(text) { return wrapTag('td', translateValue(text)) }
+function getTh(text) { return wrapTag('th', text) }
+function getThead(text) { return wrapTag('thead', text) }
+function getTfoot(text) { return wrapTag('tfoot', text) }
+
+function getSelect(text, id_in) {
+  return wrapTag('select', text, null, id_in)
+}
+
+function getYearHtmlSelect(id_in) {
+  return getSelect( SELECT_YEAR.map(year => getOption(year)).join(''), id_in)
+}
+
+function getMonthHtmlSelect(id_in) {
+  return getSelect( SELECT_MONTH.map(month => getOption(month)).join(''), id_in )
+}
+
+
+function getDayHtmlSelect(id_in) {
+  return getSelect( SELECT_DAY.map(day => getOption(day)).join(''), id_in )
+}
+
+function getTheads(fields_in){
+  return getThead(
+    fields_in.map( field => getTh(field)).join('')
+  )
+}
+
+function getTfoots(fields_in){
+  return getTheads(fields_in)
+}
+
+function getTbodys(datas_in){
+  return datas_in
+    .map(data => data.map(value => getTd(value)).join(''))
+    .map(td_in => getTr(td_in))
+    .join('')
+}
+
+function getTableContentFromJson(json_in){
+
+  let thead = getTheads(json_in.fields)
+  let tfoot = getTfoots(json_in.fields)
+  let tbody = getTbodys(json_in.data)
+  return `<table class="table is-fullwidth is-hoverable is-narrow is-bordered">
+    ${thead}
+    ${tbody}
+    ${tfoot}
+  </table>`
+}
+
+function getForm(content, form_id){
+  return `<form id=${form_id}>${content}</form>`
+}
+
+function helloworld(e){
+  e.preventDefault()
+  console.log('helloworld')
+  alert('helloworld')
+}
--- data.gov.hk/香港天文台 開放數據應用程式介面/test_interface/opendata_api.js
+++ data.gov.hk/香港天文台 開放數據應用程式介面/test_interface/opendata_api.js
@@ -4,18 +4,35 @@ const ENGLISH = 'en'
 const TRADITIONAL_CHINESE = 'tc'
 const SIMPLIFIED_CHINESE = 'sc'
 
+
+const POS_TIMES_AND_HEIGHTS_OF_HIGH_AND_LOW_TIDES = 0
+const POS_TIMES_OF_SUNRISE_SUN_TRANSIT_AND_SUNSET = 1
+const POS_TIMES_OF_MOONRISE_MOON_TRANSIT_AND_MOONSET = 2
+const POS_CLOUD_TO_GROUND_AND_CLOUD_TOCLOUD_LIGHTNING_COUNT = 3
+const POS_LATEST_10_MINUTE_MEAN_VISIBILITY = 4
+const POS_DAILY_MEAN_TEMPERATURE = 5
+const POS_DAILY_MAXIMUM_TEMPERATURE = 6
+const POS_DAILY_MINIMUM_TEMPERATURE = 7
+
+
+const POS_REQ_CONFIG = 0
+const POS_DATA_TYPE = 1
+const POS_TITLE = 2
+const POS_FORM_TEXT = 3
+const POS_FORM_ID = 4
+
 // 開放數據（氣象及天氣資訊）應用程式介面
 const OPEN_DATA_API = {
   dataType: {
-    HOURLY_HEIGHTS_OF_TIDES: 'HHOT',
     TIMES_AND_HEIGHTS_OF_HIGH_AND_LOW_TIDES: 'HLT',
     TIMES_OF_SUNRISE_SUN_TRANSIT_AND_SUNSET: 'SRS',
     TIMES_OF_MOONRISE_MOON_TRANSIT_AND_MOONSET: 'MRS',
+    CLOUD_TO_GROUND_AND_CLOUD_TOCLOUD_LIGHTNING_COUNT: 'LHL',
+    LATEST_10_MINUTE_MEAN_VISIBILITY: 'LTMV',
     DAILY_MEAN_TEMPERATURE: 'CLMTEMP',
     DAILY_MAXIMUM_TEMPERATURE: 'CLMMAXT',
     DAILY_MINIMUM_TEMPERATURE: 'CLMMINT',
-    LATEST_10_MINUTE_MEAN_VISIBILITY: 'LTMV',
-    CLOUD_TO_GROUND_AND_CLOUD_TOCLOUD_LIGHTNING_COUNT: 'LHL'
+    // HOURLY_HEIGHTS_OF_TIDES: 'HHOT',
   },
   rformat: {
     json: 'json',
@@ -45,17 +62,16 @@ let lang = OPEN_DATA_API.lang
 let rformat = OPEN_DATA_API.rformat
 let station = OPEN_DATA_API.station
 
-
-let test_list = [
+let api_test_list = [
   [ {
       dataType: data_type.TIMES_AND_HEIGHTS_OF_HIGH_AND_LOW_TIDES,
       rformat: rformat.json,
       station: station.CHEUNG_CHAU,
-      year: 2020
+      year: 2019, month: 1, day: 1
     },
     data_type.TIMES_AND_HEIGHTS_OF_HIGH_AND_LOW_TIDES,
     '漲潮和退潮的時間和高度',
-    '<p>helloworld</p>'
+    getDateForm('tide_form'), 'tide_form'
   ],
 
   [ {
@@ -66,7 +82,8 @@ let test_list = [
       day: 1
     },
     data_type.TIMES_OF_SUNRISE_SUN_TRANSIT_AND_SUNSET,
-    '日出、日中天、日落時間'
+    '日出、日中天、日落時間',
+    getDateForm('sun_form'),'sun_form'
   ],
   [ {
       dataType: data_type.TIMES_OF_MOONRISE_MOON_TRANSIT_AND_MOONSET,
@@ -76,7 +93,8 @@ let test_list = [
       day: 1
     },
     data_type.TIMES_OF_MOONRISE_MOON_TRANSIT_AND_MOONSET,
-    '月出、月中天、月落時間'
+    '月出、月中天、月落時間',
+    getDateForm('moon_form'),'moon_form'
   ],
   [ {
       dataType: data_type.CLOUD_TO_GROUND_AND_CLOUD_TOCLOUD_LIGHTNING_COUNT,
@@ -98,32 +116,104 @@ let test_list = [
   [ {
       dataType: data_type.DAILY_MEAN_TEMPERATURE,
       rformat: rformat.json,
-      station: station.CHEUNG_CHAU
+      station: station.CHEUNG_CHAU,
+      year: 2019,
+      month: 1,
+      day:1
     },
     data_type.DAILY_MEAN_TEMPERATURE,
-    '日平均氣溫'
+    '日平均氣溫',
+    getDateForm('temp_avg_form'),'temp_avg_form'
   ],
 
   [ {
       dataType: data_type.DAILY_MAXIMUM_TEMPERATURE,
       rformat: rformat.json,
-      station: station.CHEUNG_CHAU
+      station: station.CHEUNG_CHAU,
+      year: 2019,
+      month: 1,
+      day:1
     },
     data_type.DAILY_MAXIMUM_TEMPERATURE,
-    '日最高氣溫'
+    '日最高氣溫',
+    getDateForm('temp_high_form'),'temp_high_form'
   ],
 
   [ {
       dataType: data_type.DAILY_MINIMUM_TEMPERATURE,
       rformat: rformat.json,
-      station: station.CHEUNG_CHAU
+      station: station.CHEUNG_CHAU,
+      year: 2019,
+      month: 1,
+      day:1
     },
     data_type.DAILY_MINIMUM_TEMPERATURE,
-    '日最低氣溫'
+    '日最低氣溫',
+    getDateForm('temp_low_form'),'temp_low_form'
   ],
 
 
 ]
+
+function getRandomNum(){
+  return Math.random().toString().slice(2,6)
+}
+
+
+
+function getDateSelectors(form_id)
+{
+  return `
+  <div class="select">
+    ${getYearHtmlSelect(`year`)}
+  </div>
+
+  <div class="select">
+    ${getMonthHtmlSelect(`month`)}
+  </div>
+
+  <div class="select">
+    ${getDayHtmlSelect(`day`)}
+  </div>
+  `
+}
+
+function getDateForm(form_id_in){
+  return `
+  ${getDateSelectors(form_id_in)}
+  <button class="button is-link" style="margin-top: 10px;">Get</button>
+  `
+}
+
+function getTideForm(sun_form_id){
+  return `
+  ${getDateSelectors(sun_form_id)}
+  <button class="button is-link" style="margin-top: 10px;">Get</button>
+  `
+}
+function getSunForm(sun_form_id){
+  return `
+  ${getDateSelectors(sun_form_id)}
+  <button class="button is-link" style="margin-top: 10px;">Get</button>
+  `
+}
+
+function getYearSelector( id_in ) {
+  return `
+<div class="control has-icons-left">
+  <div class="select">
+    <select>
+      <option selected>Country</option>
+      <option>Select dropdown</option>
+      <option>With options</option>
+    </select>
+  </div>
+  <span class="icon is-left">
+    <i class="fas fa-globe"></i>
+  </span>
+</div>
+`
+}
 
 function getResTableHtmlId( data_type_in ) {
   return `opendataapi-${data_type_in}-table-placeholder`
@@ -154,7 +244,7 @@ function fetchGet( req_in, datatype_in ) {
   fetch( req_in )
     .then( res => res.json() )
     .then( res_json => {
-
+      console.log('fetchGet', res_html_id)
       document.querySelector( `#${res_html_id}` ).innerHTML = `<pre>${JSON.stringify(res_json)}</pre>`
 
       if ( Object.keys( res_json ).includes( 'data' ) ) {
@@ -170,8 +260,12 @@ function getEle( sel_in ) {
 }
 
 function getWeather( req_in, datatype_in ) {
+  console.log('datatype_in',getReqHtmlId( datatype_in ))
   let req_html_id = getReqHtmlId( datatype_in )
   let req_string = getRequestString( req_in )
+
+  console.log(datatype_in)
+  console.log(getReqHtmlId( datatype_in ))
 
   getEle( `#${req_html_id}` ).innerHTML = req_string
   fetchGet( req_string, datatype_in )
@@ -180,7 +274,10 @@ function getWeather( req_in, datatype_in ) {
 function genHtml( test_list ) {
   return test_list.map( x => {
     return `<div class="test_set">
-    <h3 class="subtitle is-3">${x[2]}</h3>
+    <div style="width: 100%;">
+      <h3 class="subtitle is-3 has-text-link" style="text-decoration: underline;">${x[2]}</h3>
+      ${x[3] == undefined ? '' : getForm(x[3], x[4])}
+    </div>
     <div id="${getReqHtmlId(x[ 1 ])}"></div>
     <div id="${getResHtmlId(x[ 1 ])}"></div>
     <div id="${getResTableHtmlId(x[ 1 ])}"></div>
@@ -189,17 +286,54 @@ function genHtml( test_list ) {
   } ).join( '' )
 }
 
-getEle( '#result-sheet' ).innerHTML = genHtml( test_list )
-
-test_list.forEach( x => {
-
-  data_type = x[ 1 ]
-
-  getWeather( x[ 0 ], data_type )
-} )
+getEle( '#result-sheet' ).innerHTML = genHtml( api_test_list )
 
 
 
+function getPosInTestList(data_type_in){
+  return
+}
+
+document.addEventListener( "DOMContentLoaded", function () {
+  // '#tide_form', current_pos,
+
+  api_test_list.forEach( x => {
+
+    data_type = x[ 1 ]
+
+    getWeather( x[ 0 ], data_type )
+  } )
+
+
+  for(api_test_idx of _.range(api_test_list.length)){
+    // console.log('current_form_id', test_list[current_form_id][4])
+
+    // check if user input available
+    if (api_test_list[api_test_idx][POS_FORM_ID] != undefined){
+
+      let current_form_id = api_test_list[api_test_idx][POS_FORM_ID]
+      let current_form = getEle(`#${current_form_id}`)
+
+      let test = api_test_list[api_test_idx][POS_DATA_TYPE]
+
+      let test_idx = api_test_idx
+
+      current_form.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        let updated_api_test_req = api_test_list[test_idx]
+
+        updated_api_test_req[POS_REQ_CONFIG].year = current_form.year.value
+        updated_api_test_req[POS_REQ_CONFIG].month = current_form.month.value
+        updated_api_test_req[POS_REQ_CONFIG].day = current_form.day.value
+
+        getWeather(updated_api_test_req[POS_REQ_CONFIG], test)
+      })
+
+    }
+
+  }
+})
 
 
 function sayResult( result_in ) {
@@ -221,5 +355,7 @@ function test_all() {
 }
 
 test_all()
+
+
 
 console.log( 'hello opendata_api.js' )
