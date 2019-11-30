ce0f4d30a2d947ca2b80dc50c756653df1dd3558
louiscklaw <louiscklaw@gmail.com>
2019-11-30 14:05:36 +0800

    update,

--- doc_gen/src/components/ApiListCard.js
+++ doc_gen/src/components/ApiListCard.js
@@ -20,53 +20,18 @@ class ApiDetailCard extends Component {
     let package_to_check = this.props.package_to_check
     let json_to_fetch = `assets/api_details/${package_to_check}.json`
     this.setState({isLoading: true})
-
-    // fetch(json_to_fetch)
-    //   .then(res => res.json())
-    //   .then(res_json => {
-    //     let result = res_json.result
-
-    //     this.setState({
-    //       isLoading: false,
-    //       json_url: json_to_fetch,
-
-    //       api_detail: res_json.result
-    //     })
-    //     console.log('maintainer', result.maintainer)
-    //   })
   }
 
-  getApiDetail1(package_name) {
-    let state = this.state
-    let api_detail = state.api_detail
+  getApiDescription(){
+    const {json_to_list, package_name} = this.props
+    let {title, name, notes} = json_to_list.result
 
-    if (api_detail != null && Object.keys(api_detail).includes('maintainer')){
-      return (
-        <div>
-          <div className="text-details">
-            {/* <p>{this.state.api_detail}</p> */}
-            <h3 className="is-half title">{api_detail.resources[0].name}</h3>
-            <p>{api_detail.resources[0].description}</p>
-
-            <p>{api_detail.maintainer}</p>
-            <p>{api_detail.update_frequency}</p>
-            <a href={state.json_url}>origional json</a>
-          </div>
-          <div className="tags">
-
-          </div>
-          <div className="data-dictionary">
-            <a href={api_detail.data_dictionary}>data dictionary</a>
-          </div>
-        </div>
-      )
+    if (title === notes){
+      return ''
     }else{
-      return (
-        <div>
-          <p>no detail provided</p>
-        </div>
-      )
+      return chopLongString(notes)
     }
+
   }
 
   getApiDetail(json_in, package_name){
@@ -85,8 +50,7 @@ class ApiDetailCard extends Component {
       <div className="api-detail">
         <Card>
           <p>{title}</p>
-          {/* <p>{name}</p> */}
-          <p>{chopLongString(notes)}</p>
+          <p>{this.getApiDescription()}</p>
 
           <TagList tag_list={group_names}/>
           <Link to={`api_detail/${package_name}`} >detail</Link>
